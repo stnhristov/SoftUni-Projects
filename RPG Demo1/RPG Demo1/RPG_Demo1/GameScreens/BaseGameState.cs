05886650e421ce2989348b2a1a9b52830dfa2f3a
stnhristov <stnhristov@yahoo.com>
2015-06-16 14:42:17 +0300

    new demo of tile engine rpg game

---
+++ RPG Demo1/RPG Demo1/RPG_Demo1/GameScreens/BaseGameState.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using XRpgLibrary;
+using XRpgLibrary.Controls;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RPG_Demo1.GameScreens
+{
+    public abstract partial class BaseGameState:GameState
+    {
+        #region Fields Region
+
+        protected Game1 GameRef;
+        protected ControlManager ControlManager;
+        protected PlayerIndex PlayerIndexInControl;
+
+        #endregion
+
+        #region Properties Region
+        #endregion
+        #region Constructor Region
+        public BaseGameState(Game game, GameStateManager manager) : base(game, manager) 
+        {
+            GameRef = (Game1)game;
+            PlayerIndexInControl = PlayerIndex.One;
+        }
+        #endregion
+
+        #region XNA Method Region
+
+        protected override void LoadContent()
+        {
+            ContentManager Content = Game.Content;
+            SpriteFont menuFont = Content.Load<SpriteFont>("Fonts/ControlFont");
+            ControlManager = new ControlManager(menuFont);
+            base.LoadContent();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+        #endregion
+    }
+}
---
+++ RPG Demo1/RPG Demo1/RPG_Demo1/GameScreens/TitleScreen.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+using XRpgLibrary;
+using XRpgLibrary.Controls;
+namespace RPG_Demo1.GameScreens
+{
+    public class TitleScreen:BaseGameState
+    {
+        #region Field Region
+
+        Texture2D backgroundImage;
+        LinkLabel startLabel;
+
+        #endregion
+
+        #region Constructor Region
+
+        public TitleScreen(Game game, GameStateManager manager) : base(game, manager) 
+        {
+
+        }
+
+        #endregion
+
+        #region XNA Method Region
+
+        protected override void LoadContent()
+        {
+            ContentManager Content = GameRef.Content;
+
+            backgroundImage = Content.Load<Texture2D>("Backgrounds/menubackground");
+
+            base.LoadContent();
+
+            startLabel = new LinkLabel();
+            startLabel.Position = new Vector2(360, 420);
+            startLabel.Text = @"Press ""Enter""";
+            startLabel.Color = Color.White;
+            startLabel.TabStop = true;
+            startLabel.HasFocus = true;
+            startLabel.Selected += new EventHandler(startLabel_Selected);
+
+            ControlManager.Add(startLabel);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            ControlManager.Update(gameTime, PlayerIndex.One);
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            GameRef.SpriteBatch.Begin();
+
+            base.Draw(gameTime);
+
+            GameRef.SpriteBatch.Draw(backgroundImage, GameRef.ScreenRectangle, Color.White);
+            ControlManager.Draw(GameRef.SpriteBatch);
+            GameRef.SpriteBatch.End();
+        }
+        #endregion
+
+        #region Title Screen Methods
+
+        private void startLabel_Selected(object sender, EventArgs e) 
+        {
+            StateManager.PushState(GameRef.StartMenuScreen);
+        }
+
+        #endregion
+    }
+}
